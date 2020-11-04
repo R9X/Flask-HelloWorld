de3290c6022980f73925faa0fd2d2be0335c89af
Paulo <paulohenrique9400@gmail.com>
2020-11-04 14:17:52 +0000

    Fixed

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return '<h1>Hello World</h1>'
-
-  if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+ return '<h1>Hello World!</h1>'
+ 
+if __name__ == "__main__":
+ port = int(os.environ.get("PORT", 5000))
+ app.run(host='0.0.0.0', port=port)
